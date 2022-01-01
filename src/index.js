9680c7dea81b37b690439752878da7a5dafd0384
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-01-01 16:25:32 +0200

    add exoress handlebars

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
+app.engine('.hbs', engine({ extname: '.hbs' })); //първото .hbs е разширение за експрес, второто .hbs е за разширение на handlebars
+app.set('view engine', 'hbs'); //когато пишем 'home' да не слагаме 'home.hbs'
+app.set('views', path.resolve('./src/views')) //ako views не е първно ниво
+
+
+
+
 const port = 3000;
-app.all('/', (req, res) =>{
-    res.write(`It\'s listen on port http://localhost${port}` )
-    res.end();
+app.all('/', (req, res) => {
+    res.render('index', {layout: false})
 });
 
 app.listen(port, console.log.bind(console, `App is running on http://localhost${port}`));
