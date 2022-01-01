cf2883630de73d94e61a467c66ac6f186dc7677b
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-01-02 00:02:53 +0200

    Brouse dinamic cubes

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,9 +1,29 @@
+const uniqid = require('uniqid');
+
 class Cube {
-    constructor (name, descriptors, imageUrl, difficulty){
-        this.name = name;
-        this.descriptors = descriptors;
-        this.imageUrl = imageUrl;
-        this.difficulty = difficulty;
+    static #cubes = [
+        {
+            id: 'dsadasdas',
+            name: 'Dimo',
+            descriptors: "Strange cube",
+            imageUrl: 'wqe',
+            difficulty: '4'
+          }
+    ];
+
+    constructor(name, descriptors, imageUrl, difficulty) {
+        this.id = uniqid();
+        this.name,
+        this.descriptors,
+        this.imageUrl,
+        this.difficulty
+    }
+
+    static get cubes() {
+return Cube.#cubes.slice();
+    }
+    static add(cube) {
+        Cube.#cubes.push(cube);
     }
 }
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,29 +1,11 @@
 const Cube = require('../models/Cube');
 
-const cubeDb = [{
-    name: 'Dimo',
-    descriptors: undefined,
-    imageUrl: 'wqe',
-    difficulty: '1'
-  },
-  {
-    name: 'Ivan',
-    descriptors: undefined,
-    imageUrl: 'wqe',
-    difficulty: '1'
-  },
-  {
-    name: 'Marian',
-    descriptors: undefined,
-    imageUrl: 'wqe',
-    difficulty: '1'
-  }];
-
-const getAll = () => cubeDb.slice();
+const getAll = () => Cube.cubes;
 
 const create = (name, descriptors, imageUrl, difficulty) => {
 let cube = new Cube(name, descriptors, imageUrl, difficulty);
-cubeDb.push(cube);
+
+Cube.add(cube);
 }
 
 const cubeService = {
