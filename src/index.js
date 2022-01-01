83afa7c07705faf608776ebbb8fdae43ddee6c43
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-01-01 15:32:07 +0200

    start express app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('raboti');
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+app.all('/', (req, res) =>{
+    res.write(`It\'s listen on port http://localhost${port}` )
+    res.end();
+});
+
+app.listen(port, console.log.bind(console, `App is running on http://localhost${port}`));
